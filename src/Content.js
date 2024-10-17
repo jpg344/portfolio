3afdc80b57e4076803eac88bbc76e7c8f5190993
jpg344 <174099082+jpg344@users.noreply.github.com>
2024-10-17 15:21:19 +0200

    created basic structure

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import './Content.css';
+
+function Content() {
+  return (
+    <>
+    <main>
+        <h2>Hier kommt der Content rein.</h2>
+    </main>
+    </>
+  );
+}
+
+export default Content;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+    <footer>
+        <p>&copy; 2024 jpg344</p>
+    </footer>
+    </>
+  );
+}
+
+export default Footer;
